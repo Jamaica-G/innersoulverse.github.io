4b5ff4166f7e864d5d264a77f85a3b7087db82c9
Wee Yuanliang <quaaludes81@hotmail.com>
2023-07-25 18:20:04 +0800

    mobile

---
+++ JS/script.js
@@ -0,0 +1,4 @@
+const event1 = {title:"DINING IN THE DARK", 
+date:"06 AUG",
+url:"https://www.eventbrite.sg/e/singles-dining-in-the-dark-tickets-653722941917?aff=ebdssbdestsearch&from=6c5eb9b82ad211eeab738e25e12e3ac6"}
+

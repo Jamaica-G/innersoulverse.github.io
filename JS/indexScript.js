8a352b8eb68dfe105aaa0f89c7a937eb881ad1c1
Wee Yuanliang <quaaludes81@hotmail.com>
2023-08-14 09:59:05 +0800

    test

--- JS/indexScript.js
+++ JS/indexScript.js
@@ -1,7 +1,7 @@
 
 const events = [
     { 
-        url:"https://www.eventbrite.sg/e/singles-dining-in-the-dark-tickets-653722941917?aff=ebdssbdestsearch&from=6c5eb9b82ad211eeab738e25e12e3ac6",
+        url:"#",
         img: "./img/Banner/GREEN.png"
     },
     {   

85791d10c6952256373a56dc4e127bfc97c779c9
WeeYL <quaaludes81@hotmail.com>
2024-03-03 01:45:17 +0800

    test

--- JS/blogScript.js
+++ JS/blogScript.js
@@ -1,12 +1,21 @@
 const blogs = [
-
+  {
+    title:"Singles Private Dining 02 Mar 2024 - A party of six ",
+    text: "The private dining experience was supposed to be a one-to-one rotation session. The first week started very well with four sign-ups, and we were optimistic that we could conclude the registration sooner than expected. However, as subsequent weeks passed, only one person registered for the next four weeks, and another joined on the day of the event. In order not to continue the event, we changed the setup into a group gathering, seating the six attendees together. Surprisingly, this adjustment led to unforeseen positive outcomes. The group's interaction was fantastic, and even extended their stay by about an hour, aided by the presence of sake. We expressed gratitude to the attendees for allowing us to modify the dining arrangement, realizing that a table for six might indeed offer a more enjoyable dining experience.",
+    display:'<img style="object-fit: contain" src="../img/blog/2024/2024.03.02.PRIVATEDINE.png" width="100%" height="98%">'
+  },
+  {
+    title:"Lazy Sunday KTV 18 Feb 2024 - A lesson on over optimism" ,
+    text: "The concept of Lazy Sunday KTV emerged for me personally due to the tendency of Sundays being unproductive and spent preparing for the upcoming workweek. I thought, why not turn it into a day for people to come together and enjoy a karaoke session? The idea was well-received, and we quickly reached our self-imposed limit of 10 attendees within three weeks. Excitement built up as we anticipated a day filled with great singing and socializing. As the day approached, we prepared the venue to accommodate the expected 10 participants, even though we anticipated 2-3 no-shows. However, when the event kicked off at 4 pm, we were met with a surprise — only four attendees showed up by 4:30 pm. It was disheartening to see our seemingly popular event turn out to be a bit of a letdown. Despite this, we want to express our gratitude to those who attended for their understanding. They even lightened the mood by joking about enjoying extra opportunities to sing more songs due to the lower turnout.",
+    display:'<img style="object-fit: contain" src="../img/blog/2024/2024.02.18.KTV.png" width="100%" height="98%">'
+  },
   {
     title:"Collab with Peatix 19 Nov 2023",
     text: "We were fortunate to collaborate with Peatix when Rachel from their team invited us to explore their platform. Remarkably, we experienced immediate success on the very same day when we posted Ice-Dream event.We managed to sell two tickets bringing our total registrations to the maximum capacity of eight participants allowed for the event.",
     display:'<img style="object-fit: contain" src="../img/blog/2023/2023.11.19.peatix.jpg" width="100%" height="98%">'
   },
   {
-    title:"Ice-Dream that almost did not happend 29 Oct 2023",
+    title:"Ice-Dream 29 Oct 2023 - It almost did not happend",
     text: "When initially organizing this event, we searched for workshops willing to collaborate at a reasonable cost. Unfortunately, most options demanded a minimum of $65 per attendee with a requirement of at least 10 participants. After a few days, a workshop reached out to us, assuring affordability and support for our event. However, over the subsequent weeks, the workshop owner became unresponsive to our inquiries. When she did reply, she seemed unfamiliar with our event details, that is a big red flag. But we didn't want to cancel this event, and disappoint the attendees who trusted us and registered, we reluctantly made one final search for workshops online, with the thought that it was going to fail anyway. Just when it seemed to be the case, Ice Cream Chefs, Tom, called.  We explained our predicament, and he generously stepped in to help. We were truly grateful and moved by the presence of reliable vendors like him. Another life lesson in business of not giving up", 
     display:'<img style="object-fit: contain" src="../img/blog/2023/2023.10.29.ice.cream.chef.png" width="100%" height="98%">'
 
